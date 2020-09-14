4c41405c5f9416abcd7642832f2af65a90bae281
Allan Limae <allanlimaprojetos@gmail.com>
2020-09-14 11:57:02 -0400

    final

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const electron = require('electron');
 const { Menu } = require('electron');
-const {app, BrowserWindow} = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow; // variavel para instaciar a janela
+let commentWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
+        webPreferences:{
+            nodeIntegration: true
+        }
 
     }); // variavel aponta para uma instância da janela
 
     mainWindow.loadURL(`file://${__dirname}/main.html`);//exibe o conteudo html na janela
+    mainWindow.on('closed', () => app.quit());
     const mainMenu = Menu.buildFromTemplate(menuTemplate); // cria um menu a partir do molde
     Menu.setApplicationMenu(mainMenu); // define o menu da aplicação
+})
+
+function createCommentWindow() {
+    commentWindow = new BrowserWindow({
+        width: 500,
+        height: 300,
+        title: 'Novo comentário',
+        webPreferences:{
+            nodeIntegration:true
+        }
+    });
+
+    commentWindow.loadURL(`file://${__dirname}/comment.html`);
+    commentWindow.on('closed', () => commentWindow = null);
+}
+
+ipcMain.on('addComment', (event, comment) => {
+    mainWindow.webContents.send('addComment', comment);
+    commentWindow.close();
 })
 
 const menuTemplate = [
@@ -20,12 +44,15 @@ const menuTemplate = [
         label: 'Menu',
         submenu: [
             {
-                label: 'Adicionar comentário'
+                label: 'Adicionar comentário',
+                click() {
+                    createCommentWindow();
+                }
             },
             {
                 label: 'Sair da aplicação',
                 accelerator: 'Cmd+x',
-                click(){
+                click() {
                     app.quit();
                 }
             }
@@ -34,8 +61,25 @@ const menuTemplate = [
 ]; // molde para o menu
 
 //Para o mac
-if(process.platform === 'darwin'){
+if (process.platform === 'darwin') {
     menuTemplate.unshift({
         label: ""
     });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    menuTemplate.push(
+        {
+            label: 'Dev',
+            submenu: [
+                {
+                    label: 'Debug',
+                    accelerator: process.platform === 'win32' ? 'Ctrl+Shift+I' : 'Cmd+Alt+I',
+                    click(item, focusedWindow) {
+                        focusedWindow.toggleDevTools();
+                    }
+                }
+            ]
+        }
+    )
 }
