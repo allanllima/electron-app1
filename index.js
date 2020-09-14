a1eec6ef683864362f8112a8bd86ab30e41c61c8
Allan Limae <allanlimaprojetos@gmail.com>
2020-09-14 10:28:41 -0400

    aula 2

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const electron = require('electron');
+const { Menu } = require('electron');
 const {app, BrowserWindow} = electron;
 
-let mainWindow;
+let mainWindow; // variavel para instaciar a janela
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
 
-    });
+    }); // variavel aponta para uma instância da janela
 
-    mainWindow.loadURL(`file://${__dirname}/main.html`);
+    mainWindow.loadURL(`file://${__dirname}/main.html`);//exibe o conteudo html na janela
+    const mainMenu = Menu.buildFromTemplate(menuTemplate); // cria um menu a partir do molde
+    Menu.setApplicationMenu(mainMenu); // define o menu da aplicação
 })
+
+const menuTemplate = [
+
+    {
+        label: 'Menu',
+        submenu: [
+            {
+                label: 'Adicionar comentário'
+            },
+            {
+                label: 'Sair da aplicação',
+                accelerator: 'Cmd+x',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+]; // molde para o menu
+
+//Para o mac
+if(process.platform === 'darwin'){
+    menuTemplate.unshift({
+        label: ""
+    });
+}
